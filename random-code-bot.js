52849b2ad24385df88d82a3855dcab9bd1d15d53
Daniel Cuevas <46201432+TechGeekGamer@users.noreply.github.com>
2020-03-01 01:47:31 -0800

    Update bot file
    Commands now go in `/commands/`

--- random-code-bot.js
+++ random-code-bot.js
@@ -1,6 +1,35 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -12,6 +41,6 @@ client.on('error', error => {
 
 
 // login to Discord with your app's token
-client.login(token);
-var fs = require("fs");
+client.login(token);;
+
 
