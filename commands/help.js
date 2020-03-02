1b7f7cbfbf42a953ea27a1327562ad2113cc1d1a
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-03-01 19:32:21 -0800

    Fix help command

--- commands/help.js
+++ commands/help.js
@@ -12,13 +12,13 @@ module.exports = {
     if (!args.length) {
     // ...
     data.push('Here\'s a list of all my commands:');
-data.push(commands.map(command => command.name).join('\n '));
+data.push(commands.map(command => command.name).join('\n'));
 data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
 return message.author.send(data, { split: true })
 	.then(() => {
 		if (message.channel.type === 'dm') return;
-		message.reply('I\'ve sent you a DM with all my commands!');
+		message.reply('I\'ve sent you my commands in your direct messages.');
 	})
 	.catch(error => {
 		console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
