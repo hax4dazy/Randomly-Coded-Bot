31cdb4ac294cffff969c637a52882c9b718beba6
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-03-03 01:49:52 -0800

    Fix a few debugging stuff
    Kill.js updated

--- commands/kill.js
+++ commands/kill.js
@@ -2,6 +2,7 @@ module.exports = {
 	name: 'kill',
 	description: 'Kills the bot, can\'t be restarted via command.',
 	execute(message, args) {
+		return;
 		message.channel.send('Goodbye')
 		process.exit()
 	},
