950aef6345cd69b1731b1ff3d815120497ab51c5
hax4dazy <Uitvinder.gdm@gmail.com>
2020-03-03 10:41:13 +0100

    fixed untracked files

--- commands/reload.js
+++ commands/reload.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'reload',
 	description: 'Reloads a command',
 	execute(message, args) {
-        message.channel.send('This command is currently unavailable.')
+        message.channel.send('@Daniel C#0818 please fix this command.')
         return;
 	},
 };
