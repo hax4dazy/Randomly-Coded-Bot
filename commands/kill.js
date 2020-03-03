b1c03fdd2199201ab4d8448844bd347ef731b4ee
Daniel Cuevas <46201432+DanielC-08@users.noreply.github.com>
2020-03-02 23:15:43 -0800

    Fixing my crappy code

--- commands/kill.js
+++ commands/kill.js
@@ -3,6 +3,6 @@ module.exports = {
 	description: 'Kills the bot, can\'t be restarted via command.',
 	execute(message, args) {
 		message.channel.send('Goodbye')
-		await process.exit()
+		process.exit()
 	},
 };
